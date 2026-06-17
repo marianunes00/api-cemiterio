b23c22ae0e4fff4883a85a9c62f61c5d7e73f76c
Maria Valéria Matias Nunes <matiasvaleria4504@gmail.com>
2026-06-17 17:28:18 -0300

    Configuração inicial do projeto Spring Boot

---
+++ api-cemiterio/src/main/java/br/com/ifsertao/apicemiterio/ApiCemiterioApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifsertao.apicemiterio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCemiterioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCemiterioApplication.class, args);
+	}
+
+}
